352c03261b86b9974cb02be67285328f408edb20
unknown <irfanjamesway@yahoo.co.id>
2022-03-29 10:51:06 +0700

    Init

---
+++ contracts/MasterFarmer.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.4.22 <0.9.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MasterFarmer is Ownable, Factory {
+  constructor() public {
+  }
+}
---
+++ test/test.js
@@ -0,0 +1,13 @@
+const Test = artifacts.require("Test");
+
+/*
+ * uncomment accounts to access the test accounts made available by the
+ * Ethereum client
+ * See docs: https://www.trufflesuite.com/docs/truffle/testing/writing-tests-in-javascript
+ */
+contract("Test", function (/* accounts */) {
+  it("should assert true", async function () {
+    await Test.deployed();
+    return assert.isTrue(true);
+  });
+});
